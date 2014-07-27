f2db494ad1b2cc6bbfcec33df459426078c496e4
Sonu Kumar <contact@tudostack.com>
2014-07-27 12:28:38 +0530

    fixes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,8 @@
 (function (angular){
 "use strict;"
 	var app = angular.module('bargain', ['ngRoute', 'ngResource', 'LocalStorageModule']);
-	app.config(['$routeProvider', '$httpProvider', '$resourceProvider', function($routeProvider , $httpProvider, $resourceProvider){
-		$httpProvider.defaults.useXDomain = true;
-		$resourceProvider.defaults.stripTrailingSlashes = false;
+	
+	app.config(['$routeProvider', function($routeProvider){
 		$routeProvider
 			.when('/',{
 				templateUrl:'app/views/partials/chatview.html',
@@ -17,6 +16,15 @@
 				redirectTo: '/'
 			});
 	}]);
+
+	app.config(['$httpProvider', function($httpProvider){
+	  $httpProvider.defaults.useXDomain = true;
+	}])
+
+	app.config(['$resourceProvider', function($resourceProvider){
+	  $resourceProvider.defaults.stripTrailingSlashes = false;
+	}])
+
 	app.config(['localStorageServiceProvider', function(localStorageServiceProvider){
 	  localStorageServiceProvider.setPrefix('bargain');
 	}])
