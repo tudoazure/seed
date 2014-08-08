768071d6eaab7637e0c723c44b7d93d998643c38
Sonu Kumar <contact@tudostack.com>
2014-08-09 01:13:44 +0530

    test

--- app/scripts/directives/promocode-calculator/promocode-calculator.js
+++ app/scripts/directives/promocode-calculator/promocode-calculator.js
@@ -9,19 +9,21 @@
         // replace: true,
         scope: false,
         link: function(scope, element, attrs) {
-          if(scope.products && scope.products[scope.activeChatUser]){
-            scope.product = scope.products[scope.activeChatUser];
-          }
-          else{
-            scope.product = {};
-            scope.product.imageUrl = "";
-            scope.product.description = "Product Information Not Available";
-            scope.product.price = "500";
-          }
-
+          // if(scope.products && scope.products[scope.activeChatUser]){
+          //   scope.product = scope.products[scope.activeChatUser];
+          // }
+          // else{
+          //   scope.product = {};
+          //   scope.product.imageUrl = "";
+          //   scope.product.description = "Product Information Not Available";
+          //   scope.product.price = "500";
+          // }
           scope.discountPC = 0;
           scope.discountABS = 0;
-          scope.discountType = "percentage";  
+          scope.discountType = "percentage"; 
+          scope.$watch(function(){return scope.activeChatUser;}, function(value) {
+            scope.product = scope.products[value];
+          });
         }
       }
     }]);
