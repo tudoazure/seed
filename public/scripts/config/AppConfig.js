a434f7bf046a04d11fbe412575739ad8bb1e9f18
Sonu Kumar <contact@tudostack.com>
2014-08-24 14:37:01 +0530

    fixes

--- public/scripts/config/AppConfig.js
+++ public/scripts/config/AppConfig.js
@@ -8,15 +8,17 @@ var Globals = {
   var catalogUri = catalogHost;
   Globals.AppConfig = {
   	ConcurrentChats : 3,
+    DefaultPromoCodeValidityDays : 3,
+    DefaultHistoryFetchDays : 7,
   	ChatHostURI : chatHostBaseURI.replace(/.*?:\/\//g, ""),
     ChatServerConnect :  chatHostBaseURI + "/accounts/connect/",
     MessageTemplates : chatHostBaseURI + "/one97/get-template-messages/",
+    GetUserHistory : chatHostBaseURI + "/one97/get-user-conversation/",
     ChatStartedUrl : chatHostBaseURI + "/one97/chat-started/",
     ChatClosedUrl : chatHostBaseURI + "/one97/chat-closed/",
     StropheConnect : chatHostBaseURI  + "/http-bind/",
     AgentPingBack : chatHostBaseURI  + "/one97/agent-pingback/",
     PromoCodeCreate : fulfillmentBaseURI + "admin/promocode/bargain",
-    DefaultPromoCodeValidity : 3,
     productUrl : catalogUri + "/v1/mobile/product/",
     logoutUrl : webUri + "/logout",
     CloseChatMessage : '{"CLSCHAT" : "chat closed"}'
